0c7473269c99fdb20b610ed61453f6f4408ac822
Edijs <mazaissuknis@gmail.com>
2023-06-19 14:27:00 +0300

    Fixed route for search page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
-Route::get('/', 'MovieController@search')->name('movies.search');
+Route::get('/search', [MovieController::class, 'search'])->name('movies.search');
+
 
 
