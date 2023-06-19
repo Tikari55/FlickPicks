c6cc03efe584b9faebee5b11baf9ab85b1f13269
Edijs <mazaissuknis@gmail.com>
2023-06-19 14:32:49 +0300

    Changed routes so that the search page is the default landing page

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
-Route::get('/search', [MovieController::class, 'search'])->name('movies.search');
+Route::get('/', function () {
+    return redirect()->route('movies.search');
+});
 
+Route::get('/search', [MovieController::class, 'search'])->name('movies.search');
 
 
